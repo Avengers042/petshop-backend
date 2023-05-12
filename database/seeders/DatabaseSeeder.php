d419a0b3e5391a339a1d3315e48825dd2fd640d8
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-05-12 17:13:28 -0300

    feat: add initial seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([AddressSeeder::class]);
+        $this->call([UserSeeder::class]);
+        $this->call([SupplierSeeder::class]);
+        $this->call([ProductSeeder::class]);
+        $this->call([PurchaseSeeder::class]);
+        $this->call([StockSeeder::class]);
     }
 }
