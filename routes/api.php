7c3a5f8c1da9cb11e074a74db852fac83ece2efa
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-05-13 12:58:03 -0300

    chore: remove commented lines

--- routes/api.php
+++ routes/api.php
@@ -16,14 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::controller(AddressController::class)->group(function () {
-//     Route::get('/index', 'index')->name('address.index');
-//     Route::post('/store', 'store')->name('address.store');
-//     Route::get('/edit/{id}', 'edit')->name('address.edit');
-//     Route::put('/update/{id}', 'update')->name('address.update');
-//     Route::delete('/destroy/{id}', 'destroy')->name('address.destroy');
-// });
-
 Route::group(['namespace' => 'App\Http\Controllers'], function() {
     Route::apiResource('purchases', PurchaseController::class);
     Route::apiResource('suppliers', SupplierController::class);
