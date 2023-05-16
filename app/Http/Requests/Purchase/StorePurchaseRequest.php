c87c1a62e67c1d3fd6fcec3abd1c9ab82b5ed40d
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-05-16 01:55:16 -0300

    chore: add custom message errors to validation

--- app/Http/Requests/Purchase/StorePurchaseRequest.php
+++ app/Http/Requests/Purchase/StorePurchaseRequest.php
@@ -27,11 +27,29 @@ class StorePurchaseRequest extends FormRequest
         ];
     }
 
+    /**
+     * Prepare the data for validation.
+     */
     protected function prepareForValidation()
     {
         $this->merge([
             'PRODUCT_ID' => $this->productId,
             'USER_ID' => $this->userId,
         ]);
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages() : array
+    {
+        $messages = array(
+            'productId.required' => 'Produto inválido',
+            'userId.required' => 'Usuário inválido',
+        );
+
+        return $messages;
     }
 }
--- app/Http/Requests/Purchase/UpdatePurchaseRequest.php
+++ app/Http/Requests/Purchase/UpdatePurchaseRequest.php
@@ -27,6 +27,9 @@ class UpdatePurchaseRequest extends FormRequest
         ];
     }
 
+    /**
+     * Prepare the data for validation.
+     */
     protected function prepareForValidation()
     {
         if ($this->productId
@@ -35,5 +38,20 @@ class UpdatePurchaseRequest extends FormRequest
                 'PRODUCT_ID' => $this->productId,
                 'USER_ID' => $this->userId,
             ]);
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages() : array
+    {
+        $messages = array(
+            'productId.required' => 'Produto inválido',
+            'userId.required' => 'Usuário inválido',
+        );
+
+        return $messages;
     }
 }
