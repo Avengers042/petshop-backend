afedb8fa4456a89ba6b8309998512a319b33799e
victo <victorpereira8889@gmail.com>
2023-05-16 23:11:50 -0300

    feat: add prepare for validation

--- app/Http/Requests/StoreStockRequest.php
+++ app/Http/Requests/StoreStockRequest.php
@@ -11,7 +11,7 @@ class StoreStockRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,17 @@ class StoreStockRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'productId' => ['sometimes', 'required'],
+            'amount' => ['sometimes', 'required'],
         ];
+    }
+
+    protected function prepareForValidation()
+    {
+        if ($this->productId && $this->amount) $this -> merge([
+            'PRODUCT_ID' => $this->productId,
+            'AMOUNT' => $this->amount,
+        ]);
+
     }
 }
