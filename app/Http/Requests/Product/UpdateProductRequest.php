59b7a5ddf554ba5f322892d5e574137d40f774d6
victo <victorpereira8889@gmail.com>
2023-05-17 12:49:38 -0300

    Add the folders for Stock and Product in request and resources

---
+++ app/Http/Requests/Product/UpdateProductRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Validator;
+use InvalidArgumentException;
+
+class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required'],
+            'description' => ['required' => "nome inválido"],
+            'supplierId' => ['required']
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        if ($this->name && $this->description && $this->supplierId)
+            $this->merge([
+                'NAME' => $this->name,
+                'DESCRIPTION' => $this->description,
+                'SUPPLIER_ID' => $this->supplierId,
+            ]);
+
+    }
+
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Nome inválido.',
+            'description.required' => 'Descrição inválida.',
+            'supplierId.required' => 'Fornecedor inválido.',
+            'supplierId.exists' => 'Fornecedor inválido.',
+        ];
+    }
+}
