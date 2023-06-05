7deaba0e701c3a323da3df46f9721e1885772e2d
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-06-05 13:23:39 -0300

    refactor: set explicit primary key

--- database/migrations/2023_05_11_00006_create_stocks_table.php
+++ database/migrations/2023_05_11_00006_create_stocks_table.php
@@ -4,16 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('stocks', function (Blueprint $table) {
-            $table->unsignedBigInteger('PRODUCT_ID')->primary();
-            $table->foreign('PRODUCT_ID')->references('PRODUCT_ID')->on('products');
+            $table->unsignedBigInteger('PRODUCT_ID');
             $table->integer('AMOUNT');
+            $table->primary('PRODUCT_ID');
+            $table->foreign('PRODUCT_ID')->references('PRODUCT_ID')->on('products');
             $table->index('PRODUCT_ID');
         });
     }
