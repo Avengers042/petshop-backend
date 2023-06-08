50cd7434bc23ab54a59a57c1e1674693332fc81c
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-06-08 11:54:32 -0300

    fix: add inverse relationships

--- app/Models/ShoppingCart.php
+++ app/Models/ShoppingCart.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ShoppingCart extends Model
 {
@@ -11,6 +12,7 @@ class ShoppingCart extends Model
 
     protected $primaryKey = 'SHOPPING_CART_ID';
     public $timestamps = false;
+    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
@@ -18,6 +20,16 @@ class ShoppingCart extends Model
      * @var array<int, string>
      */
     protected $fillable = [
-        'SHOPPING_CART_ID',
+        'SHOPPING_CART_ID'
     ];
+
+    public function product() : BelongsTo
+    {
+        return $this->belongsTo(Purchase::class, 'SHOPPING_CART_ID');
+    }
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class, 'SHOPPING_CART_ID');
+    }
 }
