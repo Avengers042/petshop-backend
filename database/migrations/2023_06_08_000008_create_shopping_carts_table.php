94f4034a4dc8c0520e62d6cac3d75ba92aed9281
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-06-10 13:37:12 -0300

    refactor: explicit the primary and index fields

--- database/migrations/2023_06_08_000008_create_shopping_carts_table.php
+++ database/migrations/2023_06_08_000008_create_shopping_carts_table.php
@@ -4,28 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateShoppingCartsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up():void
+    public function up(): void
     {
         Schema::create('shopping_carts', function (Blueprint $table) {
-            $table->unsignedBigInteger('SHOPPING_CART_ID')->primary();
+            $table->unsignedBigInteger('SHOPPING_CART_ID');
+            $table->primary('SHOPPING_CART_ID');
             $table->index('SHOPPING_CART_ID');
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down(): void
     {
         Schema::dropIfExists('shopping_carts');
     }
-}
+};
