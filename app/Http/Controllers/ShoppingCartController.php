7665af7667b12b5c012a1ea8e6408af6e0036a5a
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-06-10 13:25:54 -0300

    refactor: remove unused log

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -7,7 +7,6 @@ use App\Http\Requests\ShoppingCart\UpdateShoppingCartRequest;
 use App\Http\Resources\ShoppingCart\ShoppingCartCollection;
 use App\Http\Resources\ShoppingCart\ShoppingCartResource;
 use App\Models\ShoppingCart;
-use Illuminate\Support\Facades\Log;
 
 class ShoppingCartController extends Controller
 {
